41012f39ac0c77641292701f3305c1ad569beb91
Liao Yazhi <1594435860@qq.com>
2018-08-10 21:10:09 +0800

    独立出搜索框样式JS

--- js/category.js
+++ js/category.js
@@ -294,24 +294,3 @@
 //     sloganImg[1].style.opacity = "1";
 //   },1500)
 // }
-
-
-// 搜索效果
-var header = document.getElementsByTagName("header")[0];
-var input = header.getElementsByTagName("input");
-
-input[1].onclick = function(){
-  if(this.dataset.ok == "true"){
-    input[0].style.width= "4.4rem";
-    input[0].style.transition = "all 1s";
-    input[0].style.marginLeft = "0.62rem";
-    input[0].style.marginRight = "0.15rem";
-    this.dataset.ok = "false";
-  }else{
-    input[0].style.width = "0";
-    input[0].style.transition = "all 1s";
-    input[0].style.marginLeft = "-0.33rem";
-    input[0].style.marginRight = "0.53rem";
-    this.dataset.ok = "true";
-  }
-}
