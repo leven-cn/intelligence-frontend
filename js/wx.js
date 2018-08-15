dffcf362e40b58392ead535669185dd55c9c278e
leven-cn <leven.cn@gmail.com>
2018-08-15 10:14:59 +0800

    重构微信登录

--- js/wx.js
+++ js/wx.js
@@ -13,29 +13,48 @@ function getQueryVariable(variable) {
 }
 
 // 微信登录接口
-var _wxAuthToken = null;
 function wxlogin(){
-  localStorage.removeItem("wxAuthToken");
-  window.location.href = "/backend/wx/login/?auth=token&next=/intelligence/";
+  var token = localStorage.getItem("wxAuthToken");
+  if(token == null){
+    token = getQueryVariable("wxAuthToken");
+    if(token == null){
+      _wxlogin();
+    }else{
+      localStorage.setItem("wxAuthToken", token);
+    }
+  }
+  return token;
 }
 
-function wxpay(_product, _fee, _d){
-  var _xhr = new XMLHttpRequest();
-  _xhr.open("POST", "/backend/order/api/order/?payment=wx", true);
-  _xhr.setRequestHeader("Content-Type", "application/json");
-  _xhr.setRequestHeader("Authorization", _wxAuthToken);
-  _xhr.onreadystatechange = function(){
-    if (_xhr.readyState==4){
-      var _status = _xhr.status;
-      if(_status == 200){
-        var _data = JSON.parse(_xhr.responseText);
-        if(_data.ok){
-          console.log(_data.params);
+function _wxlogin(){
+  localStorage.removeItem("wxAuthToken");
+  window.location.href = "/backend/wx/login/?auth=token&next="
+      + window.location.pathname + window.location.search
+      + window.location.hash;
+}
+
+function wxpay(product, fee, details){
+  var token = localStorage.getItem("wxAuthToken");
+  if(token == null){
+    _wxlogin();
+  }
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/backend/order/api/order/?payment=wx", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", token);
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4){
+      var status = xhr.status;
+      if(status == 200){
+        var data = JSON.parse(xhr.responseText);
+        if(data.ok){
+          console.log(data.params);
 
           // 微信支付弹框
           WeixinJSBridge.invoke(
             'getBrandWCPayRequest',
-            _data.params,
+            data.params,
             function(res){
               if(res.err_msg == "get_brand_wcpay_request：ok"){
                 alert('支付成功，等待处理');
@@ -43,19 +62,19 @@ function wxpay(_product, _fee, _d){
             }
           );
         }else{
-          alert(_data.msg);
+          alert(data.msg);
         }
 
-      } else if(_xhr.status == 401){
+      }else if(xhr.status == 401){
         if(confirm("微信登录超时，确认刷新页面")){
-          wxlogin();
+          _wxlogin();
         }
       }
     }
   }
-  _xhr.send(JSON.stringify({
-    "details": _d,
-    "fee" : _fee,
-    "product": _product
+  xhr.send(JSON.stringify({
+    "details": details,
+    "fee" : fee,
+    "product": product
   }));
 }
