5b4997a750f20517ee019a207b11c56a9cfb1d60
Liao Yazhi <1594435860@qq.com>
2018-08-15 10:30:30 +0800

    新增背景随机颜色

--- js/category.slide.js
+++ js/category.slide.js
@@ -7,17 +7,34 @@ function  slide(element){
   });
   element.addEventListener("touchend", function(event){
     var touchLength = event.changedTouches[0].pageX - startX;
-
+    var backgroundColorTop = [
+      "rgb(86, 196, 236)","rgb(226, 198, 85)","rgb(230, 127, 89)","rgb(153, 177, 56)","rgb(126, 109, 213)"
+    ];
+    var backgroundColorBottom = [
+      "rgb(129,212,242)","rgb(242,216,115)","rgb(233,148,118)","rgb(170,189,93)","rgb(138,123,248)"
+    ]; 
+    var backgroundColorOpacity = [
+      "rgba(86, 196, 236,1)","rgba(226, 198, 85,1)","rgba(230, 127, 89,1)","rgba(153, 177, 56,1)","rgba(126, 109, 213,1)"
+    ];
+    var backgroundColorOpacity1 = [
+      "rgba(86, 196, 236,0)","rgba(226, 198, 85,0)","rgba(230, 127, 89,0)","rgba(153, 177, 56,0)","rgba(126, 109, 213,0)"
+    ]; 
     // 右滑
     if(touchLength > 30){
       var liList = element.getElementsByTagName("li");
       var index = _currentSlideIndex(liList);
+      var random = Math.floor((Math.random()*backgroundColorTop.length));
       if(index>0){
         liList[index].className = "";
         liList[index-1].className = "active";
         leftRem += 50;
         element.style.left = _rem(leftRem);
         element.style.transition = "all 1s";
+        document.body.style.background  = "linear-gradient("+backgroundColorTop[random]+","+backgroundColorBottom[random]+")";
+        for(var i=0;i<categoryList.getElementsByTagName("li").length;i++){
+          categoryList.getElementsByTagName("li")[i].getElementsByTagName("section")[0].style.background = backgroundColorTop[random];
+        }
+        document.getElementsByTagName("div")[0].style.background = "linear-gradient(to left,"+backgroundColorOpacity[random]+","+backgroundColorOpacity1[random]+")";
       }
     }
 
@@ -25,12 +42,18 @@ function  slide(element){
     if(touchLength < -30){
       var liList = element.getElementsByTagName("li");
       var index = _currentSlideIndex(liList);
+      var random = Math.floor((Math.random()*backgroundColorTop.length));
       if(index<liList.length-1){
         liList[index].className = "";
         liList[index+1].className = "active";
         leftRem -= 50;
         element.style.left = _rem(leftRem);
         element.style.transition = "all 1s";
+        document.body.style.background  = "linear-gradient("+backgroundColorTop[random]+","+backgroundColorBottom[random]+")";
+        for(var i=0;i<categoryList.getElementsByTagName("li").length;i++){
+          categoryList.getElementsByTagName("li")[i].getElementsByTagName("section")[0].style.background = backgroundColorTop[random];
+        }
+        document.getElementsByTagName("div")[0].style.background = "linear-gradient(to left,"+backgroundColorOpacity[random]+","+backgroundColorOpacity1[random]+")";
       }
     }
   });
