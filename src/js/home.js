af51716309f02f00d20b7d57ba62bccabc40c0aa
Liao Yazhi <1594435860@qq.com>
2018-08-24 17:25:09 +0800

    微调情报个人中心页面

--- src/js/home.js
+++ src/js/home.js
@@ -1,2 +1,13 @@
 // 点击下拉出现
-var homeList = document.getElementsByTagName("ul")[0];
+var homeList = document.querySelector("ul").querySelectorAll("li");
+for(var i=0;i<homeList.length;i++){
+	homeList[i].onclick = function(ev){
+		var oEvent = ev || event;
+		console.log(this);
+	}
+	homeList[i].querySelectorAll("em")[0].onclick = function(ev){
+		var oEvent = ev || event;
+		console.log(this);
+		oEvent.cancelBubble = true; 
+	}
+}
