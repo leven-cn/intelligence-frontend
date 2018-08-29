fea638505f344654c567b22da67e9f0b9dc60ae8
Liao Yazhi <1594435860@qq.com>
2018-08-29 11:02:06 +0800

    个人中心列表修改

--- src/js/home.js
+++ src/js/home.js
@@ -3,11 +3,21 @@ var homeList = document.querySelector("ul").querySelectorAll("li");
 for(var i=0;i<homeList.length;i++){
 	homeList[i].onclick = function(ev){
 		var oEvent = ev || event;
-		console.log(this);
+    var liElementEm = this.querySelectorAll("em");
+    if(this.dataset.offon == "true"){
+      liElementEm[0].style.display = "none";
+      liElementEm[1].style.display = "block";
+      this.dataset.offon = "false";
+      this.querySelector("ul").style.display = "block";
+    }else{
+      this.querySelector("ul").style.display = "none";
+      liElementEm[0].style.display = "block";
+      liElementEm[1].style.display = "none";
+      this.dataset.offon = "true";
+    }
 	}
-	homeList[i].querySelectorAll("em")[0].onclick = function(ev){
+	homeList[i].querySelectorAll("em")[1].onclick = function(ev){
 		var oEvent = ev || event;
-		console.log(this);
 		oEvent.cancelBubble = true; 
 	}
 }
