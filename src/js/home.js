16a500f66da0239b556fc42ec79dad2a110991dd
Liao Yazhi <1594435860@qq.com>
2018-09-03 12:01:46 +0800

    调试

--- src/js/home.js
+++ src/js/home.js
@@ -86,7 +86,7 @@ function home(prefix){
                   if(deletehttp.status == 200){
                     // console.log($t);
                     console.log(deletehttp.responseText);
-                    $t.remove($t);
+                    // $t.remove($t);
                   }
                 }
               }
