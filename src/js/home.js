283f8f612ebdcf348ab3c5269b04faf47fc6ec60
Liao Yazhi <1594435860@qq.com>
2018-09-03 16:22:42 +0800

    下拉列表

--- src/js/home.js
+++ src/js/home.js
@@ -33,91 +33,93 @@ function home(prefix){
           '<img src="img/'+dataStars.techType.toLowerCase()+'.svg" alt="HTMl"/>'+
           '<h3>'+dataStars.techType+'</h3>'+
           '<em>共'+dataStars.total+'条</em>'+
-          '<em>机密档案</em>'+
+          '<em>历史档案</em>'+
           '<em>'+dataStars.unread+'</em>'+
           '</section>'+
           '<ul></ul>'+
           '</li>';
 
+          if(dataStars.total == 0){
+            alert("暂无数据")
+          }else{
+            // 点击下拉出现
+            var homeList = ulList.getElementsByClassName("homeList");
+            for(var i=0;i<homeList.length;i++){
+              homeList[i].onclick = function(ev){
+                var oEvent = ev || event;
+                var $this = this.querySelector("ul");
+                var h3Text = this.querySelector("h3").innerHTML;
+                var spinnerhttp = new XMLHttpRequest;
+                spinnerhttp.open("GET", prefix + "/rest/intelligence/?tech-type=" + h3Text, true);
+                spinnerhttp.setRequestHeader("Authorization", token);
+                spinnerhttp.send();
+                spinnerhttp.onreadystatechange = function(){
+                  if(spinnerhttp.readyState == 4){
+                    if(spinnerhttp.status == 200){
+                      var spinnerData = JSON.parse(spinnerhttp.responseText);
+                      if(spinnerData.code == 0){
+                        $this.innerHTML = "";
+                        for(var i=0; i<spinnerData.intelligence.length;i++){
+                          var intelligence = spinnerData.intelligence[i];
+                          var elemntStr = "<li";
+                          if(intelligence.isRead){
+                            elemntStr += ' class="active"';
+                          }
+                          elemntStr += '><a href="details.html"><em>'+intelligence.version+' 版本更新</em><em>'+intelligence.releaseTime+'</em></a></li>';
+                          $this.innerHTML += elemntStr;
+                        }
+                      }else{
+                        alert(spinnerData.msg);
+                      }
+                    }
+                  }
+                }
+                var liElementEm = this.querySelectorAll("em");
+                if(this.dataset.offon == "true"){
+                  liElementEm[0].style.display = "none";
+                  liElementEm[1].style.display = "block";
+                  this.dataset.offon = "false";
+                  this.querySelector("ul").style.display = "block";
+                }else{
+                  this.querySelector("ul").style.display = "none";
+                  liElementEm[0].style.display = "block";
+                  liElementEm[1].style.display = "none";
+                  this.dataset.offon = "true";
+                }
+              }
+              homeList[i].querySelectorAll("em")[1].onclick = function(ev){
+                var oEvent = ev || event;
+                oEvent.cancelBubble = true; 
+              }
+              var deleteImg = homeList[i].getElementsByClassName("delete");
+              for(var j=0;j<deleteImg.length;j++){
+                deleteImg[j].onclick = function(ev){
+                  var $t = this.parentElement;
+                  var oEvent = ev || event;
+                  oEvent.cancelBubble = true; 
+                  var h3Text = this.parentElement.querySelector("h3").innerHTML;
+                  var deletehttp = new XMLHttpRequest;
+                  deletehttp.open("DELETE", prefix + "/rest/home/?tech-type=" + h3Text, true);
+                  deletehttp.setRequestHeader("Authorization", token);
+                  deletehttp.send();
+                  deletehttp.onreadystatechange = function(){
+                    if(deletehttp.readyState == 4){
+                      if(deletehttp.status == 200){
+                        $t.remove($t);
+                        if(homeList.length == 0){
+                          articleElement.style.display = "block";
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+
         } 
         if(data.stars.length == 0){
           articleElement.style.display = "block";
-        }
-        
-        // 点击下拉出现
-        // var homeList = ulList.querySelectorAll("li"); intelligence
-        var homeList = ulList.getElementsByClassName("homeList");
-        for(var i=0;i<homeList.length;i++){
-          homeList[i].onclick = function(ev){
-            var oEvent = ev || event;
-            var $this = this.querySelector("ul");
-            var h3Text = this.querySelector("h3").innerHTML;
-            var spinnerhttp = new XMLHttpRequest;
-            spinnerhttp.open("GET", prefix + "/rest/intelligence/?tech-type=" + h3Text, true);
-            spinnerhttp.setRequestHeader("Authorization", token);
-            spinnerhttp.send();
-            spinnerhttp.onreadystatechange = function(){
-              if(spinnerhttp.readyState == 4){
-                if(spinnerhttp.status == 200){
-                  var spinnerData = JSON.parse(spinnerhttp.responseText);
-                  if(spinnerData.code == 0){
-                    $this.innerHTML = "";
-                    for(var i=0; i<spinnerData.intelligence.length;i++){
-                      var intelligence = spinnerData.intelligence[i];
-                      var elemntStr = "<li";
-                      if(intelligence.isRead){
-                        elemntStr += ' class="active"';
-                      }
-                      elemntStr += '><a href="details.html"><em>'+intelligence.version+' 版本更新</em><em>'+intelligence.releaseTime+'</em></a></li>';
-                      console.log(elemntStr);
-                      $this.innerHTML += elemntStr;
-                    }
-                  }else{
-                    alert(spinnerData.msg);
-                  }
-                }
-              }
-            }
-            var liElementEm = this.querySelectorAll("em");
-            if(this.dataset.offon == "true"){
-              liElementEm[0].style.display = "none";
-              liElementEm[1].style.display = "block";
-              this.dataset.offon = "false";
-              this.querySelector("ul").style.display = "block";
-            }else{
-              this.querySelector("ul").style.display = "none";
-              liElementEm[0].style.display = "block";
-              liElementEm[1].style.display = "none";
-              this.dataset.offon = "true";
-            }
-          }
-          homeList[i].querySelectorAll("em")[1].onclick = function(ev){
-            var oEvent = ev || event;
-            oEvent.cancelBubble = true; 
-          }
-          var deleteImg = homeList[i].getElementsByClassName("delete");
-          for(var j=0;j<deleteImg.length;j++){
-            deleteImg[j].onclick = function(ev){
-              var $t = this.parentElement;
-              var oEvent = ev || event;
-              oEvent.cancelBubble = true; 
-              var h3Text = this.parentElement.querySelector("h3").innerHTML;
-              var deletehttp = new XMLHttpRequest;
-              deletehttp.open("DELETE", prefix + "/rest/home/?tech-type=" + h3Text, true);
-              deletehttp.setRequestHeader("Authorization", token);
-              deletehttp.send();
-              deletehttp.onreadystatechange = function(){
-                if(deletehttp.readyState == 4){
-                  if(deletehttp.status == 200){
-                    $t.remove($t);
-                    if(homeList.length == 0){
-                      articleElement.style.display = "block";
-                    }
-                  }
-                }
-              }
-            }
-          }
         }
 
       }else if(xmlhttp.status == 401){
