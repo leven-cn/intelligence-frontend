30f4689c829645fa01f7eaab6aed4b96e1d13039
Liao Yazhi <1594435860@qq.com>
2018-09-13 12:01:37 +0800

    解决苹果手机端index页面点击事件无效的问题

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,6 @@ setInterval(function(){
   window.location.href = "home.html";
 },5000)
 
-document.onclick = function(){
+document.body.onclick = function(){
   window.location.href = "home.html";
 }
