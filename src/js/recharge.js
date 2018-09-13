47f85fc3d79e49e84e25164c2b886f8ac251de44
Liao Yazhi <1594435860@qq.com>
2018-09-13 11:01:55 +0800

    试一试按钮支付

--- src/js/recharge.js
+++ src/js/recharge.js
@@ -1,5 +1,6 @@
 // 情报交易
 var rechargeList = document.querySelector("ul").querySelectorAll("li");
+var button = document.querySelector("button");
 
 function rechargeByWxpay(){
   for(var i=0;i<rechargeList.length;i++){
@@ -11,3 +12,13 @@ function rechargeByWxpay(){
   }
 }
 wxlogin(rechargeByWxpay);
+
+function rechargeButtonWxpay(){
+  button.onclick = function(){
+    var product = this.dataset.product;
+    var fee =parseInt(this.dataset.fee);
+    wxpay("情报交易",fee,[{"name":product,"count":1}]);
+  }
+}
+wxlogin(rechargeButtonWxpay);
+
