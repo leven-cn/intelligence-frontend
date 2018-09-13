7a8cc7a8bbf17f19e5e524aa91280cbb7645e328
Liao Yazhi <1594435860@qq.com>
2018-09-13 10:18:32 +0800

    微信支付修改

--- src/js/recharge.js
+++ src/js/recharge.js
@@ -1,54 +1,14 @@
 // 情报交易
-function recharge(fee){
-    var token = localStorage.getItem("wxAuthToken");
-    if(token == null){
-      return;
-    }
-    var xmlhttp = new XMLHttpRequest;
-    xmlhttp.open("POST", "/wx/pay/?payment=wx", true);
-    xmlhttp.setRequestHeader("Authorization", token);
-    xmlhttp.setRequestHeader("Content-Type", "application/json");
-    xmlhttp.onreadystatechange = function(){
-      if(xmlhttp.readyState == 4){
-        if(xmlhttp.status == 200){
-          var data = JSON.parse(xmlhttp.responseText);
-          if(data.code != 0){
-            alert(data.msg);
-          }else{
-            console.log(data.params)
-            // 微信支付弹框
-            WeixinJSBridge.invoke(
-                'getBrandWCPayRequest',
-                data.params,
-                function(res){
-                if(res.err_msg == "get_brand_wcpay_request：ok"){
-                    alert('支付成功，等待处理');
-                }
-                }
-            );
-          }
-        }else if(xmlhttp.status == 401){
-          _wxlogin();
-        }
-      }
-    }
-    xmlhttp.send(JSON.stringify({
-      "fee":fee
-    }));
-  
-  }
+var rechargeList = document.querySelector("ul").querySelectorAll("li");
 
-  var rechargeList = document.querySelector("ul").querySelectorAll("li");
-
-  function testWxPay(){
-    for(var i=0;i<rechargeList.length;i++){
-      rechargeList[i].onclick = function(){
-          console.log(this.innerHTML);
-          var fee =parseInt(this.innerHTML);
-          console.log(typeof fee);
-          recharge(fee);
-      }
+function testWxPay(){
+  for(var i=0;i<rechargeList.length;i++){
+    rechargeList[i].onclick = function(){
+        var product = this.dataset.product;
+        var fee =parseInt(this.dataset.fee);
+        console.log(fee);
+        wxpay("情报交易",fee,[{"name":product,"count":1}]);
     }
   }
-
-  wxlogin(testWxPay);
+}
+wxlogin(testWxPay);
