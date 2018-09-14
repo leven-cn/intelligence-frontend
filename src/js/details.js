6cd3697d757e10793b9cafe85331609bcc09094e
Liao Yazhi <1594435860@qq.com>
2018-09-14 11:22:54 +0800

    详情页改进

--- src/js/details.js
+++ src/js/details.js
@@ -4,11 +4,14 @@ var sectionElement = document.querySelector("section");
 
 asideImg.onclick = function(){
 	var asideMenu = this.parentElement;
+	var asideUl = document.querySelector("aside").querySelector("ul");
 	if(asideMenu.dataset.menu == "true"){
-		document.querySelector("aside").querySelector("ul").style.right = "0";
+		asideUl.style.right = "0.38rem";
+		asideUl.style.transition="all 1s";
 		asideMenu.dataset.menu = "false";
 	}else{
-		document.querySelector("aside").querySelector("ul").style.right = "-2rem";
+		asideUl.style.transition="all 1s";
+		asideUl.style.right = "-2.7rem";
 		asideMenu.dataset.menu = "true";
 	}
 }
@@ -16,14 +19,12 @@ asideImg.onclick = function(){
 sectionElement.onclick = function(){
 	if(this.dataset.share == "true"){
 		this.style.transition="all 1s";
-		this.style.width="3.1rem";
-		setTimeout(function(){
-			document.querySelector("div").style.display = "block";
-		},1000)
+		this.style.width="3.2rem";
+		document.querySelector("div").style.display = "block";
 		this.dataset.share = "false";
 	}else{
 		this.style.transition="all 1s";
-		this.style.width="0.8rem";
+		this.style.width="0.9rem";
 		this.querySelector("div").style.display = "none";
 		this.dataset.share = "true";
 	}
