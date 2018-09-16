5c02965d5d102251d1a7e876dd15b1259d13d700
Liao Yazhi <1594435860@qq.com>
2018-09-16 15:49:23 +0800

    详情页rem单位样式修改

--- src/js/details.js
+++ src/js/details.js
@@ -6,12 +6,12 @@ asideImg.onclick = function(){
 	var asideMenu = this.parentElement;
 	var asideUl = document.querySelector("aside").querySelector("ul");
 	if(asideMenu.dataset.menu == "true"){
-		asideUl.style.right = "0.38rem";
+		asideUl.style.right = "1.8rem";
 		asideUl.style.transition="all 1s";
 		asideMenu.dataset.menu = "false";
 	}else{
 		asideUl.style.transition="all 1s";
-		asideUl.style.right = "-2.7rem";
+		asideUl.style.right = "-13rem";
 		asideMenu.dataset.menu = "true";
 	}
 }
@@ -19,12 +19,12 @@ asideImg.onclick = function(){
 sectionElement.onclick = function(){
 	if(this.dataset.share == "true"){
 		this.style.transition="all 1s";
-		this.style.width="3.2rem";
+		this.style.width="16rem";
 		document.querySelector("div").style.display = "block";
 		this.dataset.share = "false";
 	}else{
 		this.style.transition="all 1s";
-		this.style.width="0.9rem";
+		this.style.width="4.8rem";
 		this.querySelector("div").style.display = "none";
 		this.dataset.share = "true";
 	}
