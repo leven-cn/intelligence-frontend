d84c7626cf83dbe5cc6218c79d3de837eeb6cf5f
leven-cn <leven.cn@gmail.com>
2018-09-17 14:56:49 +0800

    简化充值JS

--- src/js/recharge.js
+++ src/js/recharge.js
@@ -10,15 +10,11 @@ function rechargeByWxpay(){
         wxpay("情报交易",fee,[{"name":product,"count":1}]);
     }
   }
-}
-wxlogin(rechargeByWxpay);
 
-function rechargeButtonWxpay(){
   button.onclick = function(){
     var product = this.dataset.product;
     var fee =parseInt(this.dataset.fee);
     wxpay("情报交易",fee,[{"name":product,"count":1}]);
   }
 }
-wxlogin(rechargeButtonWxpay);
-
+wxlogin(rechargeByWxpay);
