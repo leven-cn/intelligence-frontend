6002aa36a1bf8f8df812a5996e97200bc42bb7ab
Liao Yazhi <1594435860@qq.com>
2018-09-20 15:54:13 +0800

    充值页面添加弹窗

--- src/js/recharge.js
+++ src/js/recharge.js
@@ -1,6 +1,20 @@
 // 情报交易
 var rechargeList = document.querySelector("ul").querySelectorAll("li");
 var button = document.querySelector("button");
+var popupElement = document.getElementsByClassName("popup")[0];
+var deleteElement = document.getElementsByClassName("delete")[0];
+var maskElement = document.getElementsByClassName("mask")[0];
+
+button.onclick = function(){
+  popupElement.style.display = "block";
+  maskElement.style.display = "block";
+}
+
+deleteElement.onclick = function(){
+  popupElement.style.display = "none";
+  maskElement.style.display = "none";
+}
+
 
 function rechargeByWxpay(){
   for(var i=0;i<rechargeList.length;i++){
@@ -11,7 +25,7 @@ function rechargeByWxpay(){
     }
   }
 
-  button.onclick = function(){
+  popupElement.querySelectorAll("a")[0].onclick = function(){
     var product = this.dataset.product;
     var fee =parseInt(this.dataset.fee);
     wxpay("情报交易",fee,[{"name":product,"count":1}]);
